4c1afc95c30f3c98f4d238746d025eab0034691f
Sergio Garcia Neto <88346250+sergio-gn@users.noreply.github.com>
2023-11-04 09:17:22 +1100

    seo adjusts
    HDO
    - resgatar favicon - ok
    - schema com avaliação - ok
    - paginas cursos - 4
    - sitemap - ok
    - arrumar muitas h1 no site -ok
    - colocar palavra chave no titulo -ok
    - link banner -ok
    - arrumar layout banner -ok
    - ajustes css -ok
    - adicionado FAQ -ok

--- js/main.js
+++ js/main.js
@@ -130,3 +130,25 @@ jQuery(document).ready(function($) {
 
 
 });
+
+
+/***** accordion ******/
+function initAcc(elem, option){
+    document.addEventListener('click', function (e) {
+        if (!e.target.matches(elem+' .a-btn')) return;
+        else{
+            if(!e.target.parentElement.classList.contains('active')){
+                if(option==true){
+                    var elementList = document.querySelectorAll(elem+' .a-container');
+                    Array.prototype.forEach.call(elementList, function (e) {
+                        e.classList.remove('active');
+                    });
+                }            
+                e.target.parentElement.classList.add('active');
+            }else{
+                e.target.parentElement.classList.remove('active');
+            }
+        }
+    });
+    }
+initAcc('.accordion.v1', true);
